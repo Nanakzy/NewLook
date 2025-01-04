72d905348766de7fe45df5c93ba2eb37853a3905
Nanakzy <nanakzy@gmail.com>
2025-01-04 06:10:35 -0800

    my first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Header from "./components/Header";
+import ImageEditor from "./components/ImageEditor";
+
+function App() {
+  return (
+    <div className="app">
+      <Header />
+      <ImageEditor />
+    </div>
+  );
+}
+
+export default App;
